af13f3ee5c5ff5d812b5af01d914d3cc04705b71
aidanschuster <63381797+aidanschuster@users.noreply.github.com>
2020-12-09 20:53:05 -0500

    leisurely slosh

--- dev/js/fuel-slosh.js
+++ dev/js/fuel-slosh.js
@@ -2,7 +2,7 @@ import { gsap } from "gsap";
 
 export function fuelSlosh() {
     var sloshTL = gsap.timeline();
-    sloshTL.to("#Fuel", { duration: 4, translateX: 80, ease: "none", yoyo: true, repeat: 12 })
+    sloshTL.to("#Fuel", { duration: 7, translateX: 80, ease: "none", yoyo: true, repeat: 6 })
 
     return sloshTL
 }
